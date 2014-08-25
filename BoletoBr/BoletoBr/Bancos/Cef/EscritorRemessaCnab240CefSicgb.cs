a408f8a6a24d393f836fe05a6bf331a36596a151
MarcosTss <marcos.ferreira@timesharesolucoes.com>
2014-08-25 18:53:30 -0300

    Preenchendo o Header de Lote da Remessa da CAIXA

--- BoletoBr/BoletoBr/Bancos/Cef/EscritorRemessaCnab240CefSicgb.cs
+++ BoletoBr/BoletoBr/Bancos/Cef/EscritorRemessaCnab240CefSicgb.cs
@@ -34,14 +34,14 @@ namespace BoletoBr.Bancos.Cef
                 header = header.PreencherValorNaLinha(18, 18, boleto.CedenteBoleto.CpfCnpj.Length == 11 ?"01" : "02");// Tipo de Inscrição da Empresa                                //Verificar depois 
                 header = header.PreencherValorNaLinha(19, 32,   boleto.CedenteBoleto.CpfCnpj.Replace(".", "").Replace("/", "").Replace("-", ""));// Número de Inscrição da Empresa  //Verificar depois
                 header = header.PreencherValorNaLinha(33, 52, "0");// Uso Exclusivo CAIXA
-                header = header.PreencherValorNaLinha(53, 57, boleto.CedenteBoleto.ContaBancariaCedente.Agencia.PadLeft(5, '0'));// Agência Mantedora da Conta
+                header = header.PreencherValorNaLinha(53, 57, boleto.CedenteBoleto.ContaBancariaCedente.Agencia.PadLeft(5, '0'));// Agência Mantenedora da Conta
                 header = header.PreencherValorNaLinha(58, 58, boleto.CedenteBoleto.ContaBancariaCedente.DigitoConta);// Dígito Verificador da Agência                              //Verificar depois                
                 header = header.PreencherValorNaLinha(59, 64, boleto.CedenteBoleto.CodigoCedente); // Codigo Convênio
                 header = header.PreencherValorNaLinha(65, 71, "0");// Uso Exclusivo CAIXA
                 header = header.PreencherValorNaLinha(72, 72, "0");// Uso Exclusivo CAIXA
                 header = header.PreencherValorNaLinha(73, 102, boleto.CedenteBoleto.Nome.PadRight(30, ' '));// Nome da Empresa 
                 header = header.PreencherValorNaLinha(103, 132, boleto.BancoBoleto.NomeBanco);// Nome do Banco                                                                    //Verificar depois
-                header = header.PreencherValorNaLinha(133, 142, "Brancos");// Uso Exclusivo FEBRABAN / CNAB 
+                header = header.PreencherValorNaLinha(133, 142, "");// Uso Exclusivo FEBRABAN / CNAB 
                 header = header.PreencherValorNaLinha(143, 143, "");// Código Remessa/ Retorno                                                                                   // Não informado
                 header = header.PreencherValorNaLinha(144, 151,  DateTime.Now.ToString("d").Replace("/", ""));// Data de Geração do Arquivo
                 header = header.PreencherValorNaLinha(152, 157, DateTime.Now.ToString("t").Replace(":", ""));// Hora de Geração do Arquivo                                      //Verificar depois
@@ -51,8 +51,8 @@ namespace BoletoBr.Bancos.Cef
                 header = header.PreencherValorNaLinha(172, 191, "");// Para uso reservado do Banco
                 header = header.PreencherValorNaLinha(192, 211, "");// Para uso reservado da Empresa
                 header = header.PreencherValorNaLinha(212, 215, "");// Versão Aplicativo CAIXA
-                header = header.PreencherValorNaLinha(216, 240, "Brancos");// Uso Exclusivo FEBRABAN / CNAB 
-
+                header = header.PreencherValorNaLinha(216, 240, "");// Uso Exclusivo FEBRABAN / CNAB 
+                
                 return header;
             }
             catch (Exception e)
@@ -74,23 +74,23 @@ namespace BoletoBr.Bancos.Cef
                 headerlote = headerlote.PreencherValorNaLinha(10, 11, "");// Tipo de Serviço
                 headerlote = headerlote.PreencherValorNaLinha(12, 13, "00");// Uso Exclusivo FREBRABAN/CNAB
                 headerlote = headerlote.PreencherValorNaLinha(14, 16, "030");// Nº da versão do Layout do Lote
-                headerlote = headerlote.PreencherValorNaLinha(17, 17, "Brancos");// Uso Exclusivo FREBRABAN/CNAB
+                headerlote = headerlote.PreencherValorNaLinha(17, 17, " ");// Uso Exclusivo FREBRABAN/CNAB
                 headerlote = headerlote.PreencherValorNaLinha(18, 18, boleto.CedenteBoleto.CpfCnpj.Length == 11 ? "01" : "02");// Tipo de Inscrição da Empresa
                 headerlote = headerlote.PreencherValorNaLinha(19, 33, boleto.CedenteBoleto.CpfCnpj.Replace(".", "").Replace("/", "").Replace("-", ""));// Nº de Inscrição da Empresa (CPF/CNPJ)
-                headerlote = headerlote.PreencherValorNaLinha(34, 39, "" );// Código do Cedente no Banco
+                headerlote = headerlote.PreencherValorNaLinha(34, 39,  boleto.CedenteBoleto.CodigoCedente);// Código do Cedente no Banco
                 headerlote = headerlote.PreencherValorNaLinha(40, 53, "0");// Uso Exclusivo da CAIXA
-                headerlote = headerlote.PreencherValorNaLinha(54, 58, "");// Agência Mantenedora da Conta
-                headerlote = headerlote.PreencherValorNaLinha(59, 59, "");// Dígito Verificador da Conta
-                headerlote = headerlote.PreencherValorNaLinha(60, 65, "");// Código do Convênio no Banco
+                headerlote = headerlote.PreencherValorNaLinha(54, 58, boleto.CedenteBoleto.ContaBancariaCedente.Agencia.PadLeft(5, '0'));// Agência Mantenedora da Conta 
+                headerlote = headerlote.PreencherValorNaLinha(59, 59, boleto.CedenteBoleto.ContaBancariaCedente.DigitoConta);// Dígito Verificador da Conta
+                headerlote = headerlote.PreencherValorNaLinha(60, 65, boleto.BancoBoleto.CodigoBanco);// Código do Convênio no Banco
                 headerlote = headerlote.PreencherValorNaLinha(66, 72, "");// Código do Modelo Personalizado
                 headerlote = headerlote.PreencherValorNaLinha(73, 73, "0");// Uso Exclusivo da CAIXA
-                headerlote = headerlote.PreencherValorNaLinha(74, 103, "");// Nome da Empresa
+                headerlote = headerlote.PreencherValorNaLinha(74, 103, boleto.CedenteBoleto.Nome.PadRight(30, ' '));// Nome da Empresa
                 headerlote = headerlote.PreencherValorNaLinha(104, 143, "");// Mensagem 1
                 headerlote = headerlote.PreencherValorNaLinha(144, 183, "");// Mensagem 2
                 headerlote = headerlote.PreencherValorNaLinha(184, 191, "");// Número Remessa/Retorno
-                headerlote = headerlote.PreencherValorNaLinha(192, 199, "");// Data de Gravação Remessa/Retorno
+                headerlote = headerlote.PreencherValorNaLinha(192, 199, DateTime.Now.ToString("d").Replace("/", ""));// Data de Gravação Remessa/Retorno
                 headerlote = headerlote.PreencherValorNaLinha(200, 207, "");// Data do Crédito
-                headerlote = headerlote.PreencherValorNaLinha(208, 240, "Brancos");// Uso Exclusivo PREBRABAN/CNAB
+                headerlote = headerlote.PreencherValorNaLinha(208, 240, "".PadRight(40, ' '));// Uso Exclusivo PREBRABAN/CNAB
 
                 return headerlote;
             }
@@ -101,7 +101,67 @@ namespace BoletoBr.Bancos.Cef
             }
        }
         
+        public string EscreverDetalhe(Boleto boleto, int numeroRegistro)
+        {
+            string detalhe = new string(' ', 240);
+            try
+            {
+                detalhe = detalhe.PreencherValorNaLinha(1, 3, "104");// Código do Banco na Compensação
+                detalhe = detalhe.PreencherValorNaLinha(4, 7, "");// Lote De Serviço
+                detalhe = detalhe.PreencherValorNaLinha(8, 8, "3");// Tipo de Registro
+                detalhe = detalhe.PreencherValorNaLinha(9, 13, "");// Nº Sequencial do Registro no Lote
+                detalhe = detalhe.PreencherValorNaLinha(14, 14, "P");// Cód. Segmento do Registro Detalhe
+                detalhe = detalhe.PreencherValorNaLinha(15, 15, "");// Uso Exclusivo FEBRABAN/CNAB
+                detalhe = detalhe.PreencherValorNaLinha(16, 17, "");// Código de Movimento Remessa
+                detalhe = detalhe.PreencherValorNaLinha(18, 22, "");// Agência Mantenedora da Conta
+                detalhe = detalhe.PreencherValorNaLinha(23, 23, "");// Dígito Verificador da Agência
+                detalhe = detalhe.PreencherValorNaLinha(24, 29, "");// Código do Convênio no Banco
+                detalhe = detalhe.PreencherValorNaLinha(30, 37, "0");// Uso Exclusivo CAIXA
+                detalhe = detalhe.PreencherValorNaLinha(38, 40, "0");// Uso Exclusivo CAIXA
+                detalhe = detalhe.PreencherValorNaLinha(41, 42, "");// Modalidade da Carteira
+                detalhe = detalhe.PreencherValorNaLinha(43, 57, "");// Identificaçao do Título no Banco
+                detalhe = detalhe.PreencherValorNaLinha(58, 58, "");// Código da Carteira
+                detalhe = detalhe.PreencherValorNaLinha(59, 59, "");// Forma de Cadastr. do Título no Banco
+                detalhe = detalhe.PreencherValorNaLinha(60, 60, "2");// Tipo de Documento
+                detalhe = detalhe.PreencherValorNaLinha(61, 61, "");// Identificação da Emissão do Bloqueto
+                detalhe = detalhe.PreencherValorNaLinha(62, 62, "");// Identificação da Entrega do Bloqueto
+                detalhe = detalhe.PreencherValorNaLinha(63, 73, "");// Número do Documento de Cobrança
+                detalhe = detalhe.PreencherValorNaLinha(74, 77, "");// Uso Exclusivo CAIXA
+                detalhe = detalhe.PreencherValorNaLinha(78, 85, "");// Data de Vencimento do Título
+                detalhe = detalhe.PreencherValorNaLinha(86, 100, "");// Valor Nominal do Título
+                detalhe = detalhe.PreencherValorNaLinha(101, 105, "0");// Agência Encarregada da Cobrança
+                detalhe = detalhe.PreencherValorNaLinha(106, 106, "0");// Dígito Verificador da Agência
+                detalhe = detalhe.PreencherValorNaLinha(107, 108, "");// Espécia do Título
+                detalhe = detalhe.PreencherValorNaLinha(109, 109, "");// Identific. de Título Aceito/Não Aceito
+                detalhe = detalhe.PreencherValorNaLinha(110, 117, "");// Data da Emissão de Título
+                detalhe = detalhe.PreencherValorNaLinha(118, 118, "");// Código do Juros de Mora
+                detalhe = detalhe.PreencherValorNaLinha(119, 126, "");// Data do Juros de Mora
+                detalhe = detalhe.PreencherValorNaLinha(127, 141, "");// Juros de Mora por Dia/Taxa
+                detalhe = detalhe.PreencherValorNaLinha(142, 142, "");// Código do Desconto 1
+                detalhe = detalhe.PreencherValorNaLinha(143, 150, "");// Data do Desconto 1
+                detalhe = detalhe.PreencherValorNaLinha(151, 165, "");// Valor/Percentual a ser Concedido
+                detalhe = detalhe.PreencherValorNaLinha(166, 180, "");// Valor do IOF a ser Recolhido
+                detalhe = detalhe.PreencherValorNaLinha(181, 195, "");// Valor do Abatimento
+                detalhe = detalhe.PreencherValorNaLinha(196, 220, "");// Identificação do Título na Empresa
+                detalhe = detalhe.PreencherValorNaLinha(221, 221, "");// Código para Protesto
+                detalhe = detalhe.PreencherValorNaLinha(222, 223, "");// Número de Dias para Protesto
+                detalhe = detalhe.PreencherValorNaLinha(224, 224, "");// Código para Baixa/Devolução
+                detalhe = detalhe.PreencherValorNaLinha(225, 227, "");// Número de Dias para Baixa/Devolução 
+                detalhe = detalhe.PreencherValorNaLinha(228, 229, "0");// Código da Moeda
+                detalhe = detalhe.PreencherValorNaLinha(230, 239, "");// Uso Exclusivo CAIXA
+                detalhe = detalhe.PreencherValorNaLinha(240, 240, "");// Uso Exclusivo FREBRABAN/CNAB
+
+                return detalhe;
+            }
+            catch(Exception e)
+            
+           {
+        throw new Exception(String.Concat("Falha na geração do DETALHE do arquivo de REMESSA.",
+            Environment.NewLine, e));
         
+            }
+
+        }
      
         
         
@@ -116,11 +176,11 @@ namespace BoletoBr.Bancos.Cef
                 trailer = trailer.PreencherValorNaLinha(1, 3, "104");
                 trailer = trailer.PreencherValorNaLinha(4, 7, "9999");
                 trailer = trailer.PreencherValorNaLinha(8, 8, "9");
-                trailer = trailer.PreencherValorNaLinha(9, 17, "Brancos");
+                trailer = trailer.PreencherValorNaLinha(9, 17, "");
                 trailer = trailer.PreencherValorNaLinha(18, 23, "");
                 trailer = trailer.PreencherValorNaLinha(24, 29, "");
-                trailer = trailer.PreencherValorNaLinha(30, 35, "Brancos");
-                trailer = trailer.PreencherValorNaLinha(36, 240, "Brancos");
+                trailer = trailer.PreencherValorNaLinha(30, 35, "");
+                trailer = trailer.PreencherValorNaLinha(36, 240, "");
 
                 return trailer;
             }
