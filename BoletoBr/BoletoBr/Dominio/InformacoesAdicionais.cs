8c69be22e7862f481b8a7d213bf15bee365f37a6
Samuel Oliveira <sroliveira@outlook.com>
2015-02-02 15:18:06 -0200

    Update - Criação de atributo 'Informações Adicionais' no boleto.

---
+++ BoletoBr/BoletoBr/Dominio/InformacoesAdicionais.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoletoBr.Dominio
+{
+    /// <summary>
+    /// Informações Adicionais para uso livre no boleto
+    /// </summary>
+    public class InformacoesAdicionais
+    {
+        public string ProdutoServico { get; set; }
+        public int Quantidade { get; set; }
+        public decimal ValorUnit { get; set; }
+        public decimal ValorTotal { get; set; }
+        public string ValorTotalExtenso { get; set; }
+        public string InfoAdicionais { get; set; }
+    }
+}
