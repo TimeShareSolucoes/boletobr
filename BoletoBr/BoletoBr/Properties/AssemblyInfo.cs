4db2216169c646db7fa81115f036740f72389b11
Marcello Borges <marcelloaborges@gmail.com>
2015-07-30 11:37:36 -0300

    updt "correção número versão"

--- BoletoBr/BoletoBr/Properties/AssemblyInfo.cs
+++ BoletoBr/BoletoBr/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2015.7.30.1117")]
-[assembly: AssemblyFileVersion("2015.7.30.1117")]
+[assembly: AssemblyVersion("1.0.2.13")]
+[assembly: AssemblyFileVersion("1.0.2.13")]
