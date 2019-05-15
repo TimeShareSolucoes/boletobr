874a51e56b0aa8cd44c247edf4e8c313a9bed87c
Marcelo Santos <marcelo.santos@timesharesolucoes.com.br>
2019-05-15 14:30:16 -0300

    Correção escritor remessa Bradesco, tipo 2 completa com zeros

--- BoletoBr/BoletoBr/Bancos/Banestes/EscritorRemessaCnab400Banestes.cs
+++ BoletoBr/BoletoBr/Bancos/Banestes/EscritorRemessaCnab400Banestes.cs
@@ -145,7 +145,7 @@ namespace BoletoBr.Bancos.Banestes
                 detalhe = detalhe.PreencherValorNaLinha(91, 92, string.Empty.PadLeft(2, '0'));
 
                 detalhe = detalhe.PreencherValorNaLinha(93, 93, /*infoDetalhe.InscricaoPagador.BoletoBrToStringSafe().Replace(".", "").Replace("-", "").Length == 11 ? "1" : "2"*/ string.Empty.PadLeft(1, ' '));
-                detalhe = detalhe.PreencherValorNaLinha(94, 107, /*infoDetalhe.InscricaoPagador.Replace(".", "").Replace("/", "").Replace("-", "")*/string.Empty.PadLeft(14, '0'));
+                detalhe = detalhe.PreencherValorNaLinha(94, 107, /*infoDetalhe.InscricaoPagador.Replace(".", "").Replace("/", "").Replace("-", "")*/string.Empty.PadLeft(14, ' '));
 
                 detalhe = detalhe.PreencherValorNaLinha(108, 108, infoDetalhe.CarteiraCobranca == "11" ? "1" : "3");/*1 - simples | 3- Caucionada*/
                 detalhe = detalhe.PreencherValorNaLinha(109, 110, infoDetalhe.CodigoOcorrencia.Codigo.BoletoBrToStringSafe().PadLeft(2, '0'));
